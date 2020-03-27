00ddd9eb4fad33d303aab28a4b7dd36bd82d76dc
N. Sanchirico <Lnk2past@gmail.com>
2020-03-27 16:47:32 -0400

    [DART-397] - add comments and documentation (#4)
    * comments and cleanup
    
    * update readme
    
    * fix typo
    
    * update readme

--- pystreams/streams/agents.py
+++ pystreams/streams/agents.py
@@ -1,15 +1,18 @@
-from faust.streams import Stream
+from faust import StreamT
 from pystreams.app import app
 from pystreams.messages.stream_message import StreamMessage
 
 
+# create topics
 stream_in_topic = app.topic('stream.in', value_type=StreamMessage)
 stream_out_topic = app.topic('stream.out', value_type=StreamMessage)
 
 
+# create an agent subscribed to the stream.in topic. this function receives
+# events, updates them, and then forwards them to the stream.out topic
 @app.agent(stream_in_topic)
-async def stream_in(stream : Stream):
-    """Update and forward events to the stream_out agent"""
+async def stream_in(stream: StreamT):
+    """Update and forward events to the stream_out_topic topic"""
     async for event in stream:
         event.breadcrumbs.append('python-kafka-streams')
         await stream_out_topic.send(value=event)
